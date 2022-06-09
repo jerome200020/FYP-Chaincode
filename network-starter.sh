5eecb0f8623c70cba95bcc5d4108f5f8b4f5d5bf
jerome200020 <64579876+jerome200020@users.noreply.github.com>
2022-06-10 00:01:02 +0800

    amend commit
    amended directories

--- network-starter.sh
+++ network-starter.sh
@@ -20,11 +20,11 @@ cd "../test-network/"
 ./network.sh deployCC -c test -ccn cert -ccp ../fyp-chaincode/chaincode -ccl go -ccep "OR('Org1MSP.peer','Org2MSP.peer')"
 
 # Copy the connection profiles so they are in the correct organizations.
-cp "organizations/peerOrganizations/org1.example.com/connection-org1.yaml" "${DIR}gateway/"
-cp "organizations/peerOrganizations/org2.example.com/connection-org2.yaml" "${DIR}gateway/"
+cp "/organizations/peerOrganizations/org1.example.com/connection-org1.yaml" "${DIR}/gateway/"
+cp "/organizations/peerOrganizations/org2.example.com/connection-org2.yaml" "${DIR}/gateway/"
 
-cp "organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/"* "organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/User1@org1.example.com-cert.pem"
-cp "organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/"* "organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/priv_sk"
+cp "/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/"* "/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/User1@org1.example.com-cert.pem"
+cp "/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/"* "/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/priv_sk"
 
-cp "organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/"* "organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/User1@org2.example.com-cert.pem"
-cp "organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/"* "organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/priv_sk"
+cp "/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/"* "/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/User1@org2.example.com-cert.pem"
+cp "/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/"* "/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/priv_sk"
