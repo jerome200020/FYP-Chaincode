2c779ca6d761351578b7e64d2a046be99511bc70
jerome200020 <64579876+jerome200020@users.noreply.github.com>
2022-06-09 23:53:01 +0800

    update network-starter.sh
    change directories

--- network-starter.sh
+++ network-starter.sh
@@ -12,7 +12,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export FABRIC_CFG_PATH="${DIR}/../config"
 
-cd "${DIR}/../test-network/"
+cd "../test-network/"
 
 ./network.sh down
 ./network.sh up createChannel -c test -s couchdb -ca
@@ -20,11 +20,11 @@ cd "${DIR}/../test-network/"
 ./network.sh deployCC -c test -ccn cert -ccp ../fyp-chaincode/chaincode -ccl go -ccep "OR('Org1MSP.peer','Org2MSP.peer')"
 
 # Copy the connection profiles so they are in the correct organizations.
-cp "${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/connection-org1.yaml" "${DIR}/gateway/"
-cp "${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/connection-org2.yaml" "${DIR}/gateway/"
+cp "organizations/peerOrganizations/org1.example.com/connection-org1.yaml" "${DIR}gateway/"
+cp "organizations/peerOrganizations/org2.example.com/connection-org2.yaml" "${DIR}gateway/"
 
-cp "${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/"* "${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/User1@org1.example.com-cert.pem"
-cp "${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/"* "${DIR}/../test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/priv_sk"
+cp "organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/"* "organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/User1@org1.example.com-cert.pem"
+cp "organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/"* "organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/priv_sk"
 
-cp "${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/"* "${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/User1@org2.example.com-cert.pem"
-cp "${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/"* "${DIR}/../test-network/organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/priv_sk"
+cp "organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/"* "organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/signcerts/User1@org2.example.com-cert.pem"
+cp "organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/"* "organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/priv_sk"
